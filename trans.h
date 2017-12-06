27580f3831793274f164ce14aefe31f7b76ed920
彭宣童 <994684265@qq.com>
2017-12-06 16:42:20 +0800

    Add files via upload

---
+++ trans.h
@@ -0,0 +1,2 @@
+#include<iostream>
+void Re_fusion2(const char * BufferIn0,const char * BufferIn1,const char * BufferIn2,const char * BufferIn3,const char * BufferIn4,const char * BufferOut,int win_size,int class_num,float M_err);
