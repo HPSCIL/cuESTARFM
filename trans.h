81d3ffcd1264d794e8dd47bceade8b03ca452b68
彭宣童 <994684265@qq.com>
2017-12-28 20:15:38 +0800

    Delete trans.h

--- trans.h
+++
@@ -1,2 +0,0 @@
-#include<iostream>
-void Re_fusion2(const char * BufferIn0,const char * BufferIn1,const char * BufferIn2,const char * BufferIn3,const char * BufferIn4,const char * BufferOut,int win_size,int class_num,float M_err);
