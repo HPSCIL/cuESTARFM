c5db45656856c9e98027e86c3515cbedb61cebd8
彭宣童 <994684265@qq.com>
2018-01-19 17:21:39 +0800

    Add files via upload

---
+++ Windows1.1/PARAMETER.h
@@ -0,0 +1,14 @@
+//#include"cuLayer.h"
+typedef struct { 
+ // char outpath[1000];
+ // int A;
+  int class_num;
+  char G_Type[50];
+ // int pf;
+ // int pc;
+ // float L_ERR;
+  int NUM_PREDICTIONS;
+  float uncertain;
+  int NUM_PAIRS;             /* number of input data pair */ 
+  int WIN_SIZE;              /* window size determined by the MAX_SEARCH_DIS */
+} PARAMETER;
