2848046258a6df31e15ebf3ab27daf732d25fd46
彭宣童 <994684265@qq.com>
2018-04-19 16:37:46 +0800

    change

--- Windows1.1/trans.h
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-#include"PARAMETER.h"
-#include"cuLayer.h"
-void Re_fusion2(const char * BufferIn0,const char * BufferIn1,const char * BufferIn2,const char * BufferIn3,const char * BufferIn4,const char * BufferOut,int win_size,int class_num,float M_err);
-int parseParameters(char *fname, CuLayer *psensor,PARAMETER *par);
-void usage(char *command);
-void Re_fusion3(CuLayer *psensor,PARAMETER *par);
